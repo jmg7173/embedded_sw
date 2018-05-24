8a25586fd26837c9d2dea78918ca9e11d2d7c9c5
MinGyo Jung <jmg7173@gmail.com>
2018-05-24 16:02:11 +0900

    Add open/release/write/read and timer functions TODO: add headers

--- hw2/module/dev_driver.c
+++ hw2/module/dev_driver.c
@@ -4,6 +4,7 @@
 #include <asm/uaccess.h>
 
 #include "io_driver.h"
+
 #define DEV_DRIVER_MAJOR 242
 #define DEV_DRIVER_NAME "dev_driver"
 
@@ -14,7 +15,6 @@ static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-
 static struct file_operations fops = {
     .read = device_read,
     .write = device_write,
@@ -23,6 +23,98 @@ static struct file_operations fops = {
     .ioctl = device_ioctl
 };
 
+static struct timer_data{
+    struct timer_list timer;
+    int start;      // pattern
+    int cur;        // cur pattern
+    int pos;        // fnd position
+    int remain;     // iter remain
+    int gap;        // time gap
+};
+
+struct timer_data data;
+
+static int dev_usage = 0;
+
+// open
+static int device_open(struct inode *inode, struct file *file){
+    if(dev_usage){
+        return -EBUSY;
+    }
+    dev_usage = 1;
+    return 0;
+}
+
+// close
+static int device_release(struct inode *inode, struct file *file){
+    dev_usage = 0;
+    return 0;
+}
+
+// read - nop
+static ssize_t device_read(struct file *filp, char *buff, size_t len, loff_t *off){
+    printk(KERN_ALERT "This operation isn't supported.\n");
+    return -EINVAL;
+}
+
+
+static void timer_iterator(unsigned long timeout){
+    struct timer_data *p_data = (struct timer_data*)timeout;
+
+    // do something
+
+    p_data->remain--;
+    if(p_data->remain <= 0){
+        // need initialize
+        return;
+    }
+
+    data.timer.expires = get_jiffies_64() + (p_data->gap * HZ/10);
+    data.timer.data = (unsigned long)&data;
+    data.timer.function = timer_iterator;
+    add_timer(&mydata.timer);
+}
+
+
+// write
+static ssize_t device_write(struct file *filp, const char *gdata, size_t len, loff_t *offset){
+    const char *tmp = gdata;
+    int val; = *(int *)gdata;
+    int start_pos;
+    int start_val;
+    int time_gap;
+    int times;
+
+    if(copy_from_user(&val, tmp, 4)){
+        return -EFAULT;
+    }
+
+    start_pos = (val & 0xFF000000) >> 24;
+    start_val = (val & 0xFF0000) >> 16;
+    time_gap = (val & 0xFF00) >> 8;
+    times = (val & 0xFF);
+
+    printk("start position: %d\n", start_pos);
+    printk("start pattern : %d\n", start_val);
+    printk("time gap      : %d\n", time_gap);
+    printk("iteration     : %d\n", times);
+
+    del_timer_sync(&data.timer);
+
+    data.start = start_val;
+    data.pos = start_pos;
+    data.remain = times;
+    data.cur = start_val;
+    data.gap = time_gap;
+
+    data.timer.expires = get_jiffies_64() + (time_gap*HZ/10);
+    data.timer.function = timer_iterator;
+    data.timer.data = (unsigned long)&data;
+
+    add_timer(&mydata.timer);
+
+    return 1;
+}
 
 // driver init
 int dev_driver_init(void){
@@ -34,11 +126,15 @@ int dev_driver_init(void){
     }
     printk("init module, %s major number: %d\n", DEV_DRIVER_NAME, DEV_DRIVER_MAJOR);
 
+    init_timer(&(data.timer));
     return 0;
 }
 
 // driver exit
 void dev_driver_exit(void){
+    printk("dev_driver exit\n");
+    dev_usage = 0;
+    del_timer_sync(&data.timer);
     unregister_chrdev(DEV_DRIVER_MAJOR, DEV_DRIVER_NAME);
 }
 
